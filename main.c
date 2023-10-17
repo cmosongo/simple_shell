bd801499d40d5daed36ec055198e7da1a8e88088
cmosongo <charles.mosongo@strathmore.edu>
2023-10-17 12:30:16 -0700

    more shell scripts

--- main.c
+++ main.c
@@ -1,105 +1,135 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * display_welcome_message - Run display
  *
- * @argc: argument count
- * @argv: argument vector
+ *@void: no parameter
  *
- * Return: 0 on success.
+ * Return: welcome message.
  */
+void display_welcome_message(void)
+{
+	printf("Welcome to simple shell\nCTRL+D or type 'exit' to quit\n");
+}
 
-int main(void)
+/**
+ * read_command - Read commands input
+ *
+ *@void: no parameter
+ *
+ * Return: Read command.
+ */
+char *read_command(void)
 {
 	char *command = NULL;
 	size_t command_size = 0;
 	ssize_t read_chars;
-	pid_t pid ;
-	int status;
-	bool eof_detected = false;
 
-	printf("Welcome to simple shell v.1.0.0 \nPress 'CTRL +D' or type 'exit' to quit)\n\n");
-	while (!eof_detected)
-	{
-		printf("[(*°▽°*)] ");
+	printf("[(*°▽°*)] ");
 
-	/* Read the command*/
 	read_chars = getline(&command, &command_size, stdin);
 
 	if (read_chars == -1)
 	{
 		if (feof(stdin))
 		{
-			eof_detected = true;  /* EOF detected*/
-		}
-		else
+			free(command);
+			command = NULL;
+		} else
 		{
 			perror("getline");
 		}
-
-	break;
-	}
-
-	/*Remove newline character */
-	if (read_chars > 0 && command[read_chars - 1] == '\n')
-	{
+	} else
+		if (read_chars > 0 && command[read_chars - 1] == '\n')
+		{
 		command[read_chars - 1] = '\0';
-	}
+		}
 
-	/* Check if the user wants to exit */
-	if (strcmp(command, "exit") == 0)
-	{
-		printf("Exiting...\n");
-		break;
-	}
+	return (command);
+}
 
-	/* Fork a new process*/
+/**
+ * execute_command - Calls system scripts
+ *
+ * @command: command to be executed
+ *
+ * Return: no return
+ */
+void execute_command(char *command)
+{
+	pid_t pid;
+	int status;
+
+	if (command == NULL)
+		return;
+
 	pid = fork();
 
 	if (pid == -1)
 	{
 		perror("fork");
 		exit(EXIT_FAILURE);
+	} else
+	if (pid == 0)
+	{
+		char *args[4];
+
+		args[0] = "/bin/sh";
+		args[1] = "-c";
+		args[2] = command;
+		args[3] = NULL;
+
+		if (execve("/bin/sh", args, NULL) == -1)
+		{
+			perror("execve");
+			_exit(EXIT_FAILURE);
+		}
 	}
 	else
-		if (pid == 0)
-		{
-			/*Child process: execute the command using execve */
-			char *args[4];
-			args[0] = "/bin/sh";
-			args[1] = "-c";
-			args[2] = command;
-			args[3] = NULL;
+	{
+		waitpid(pid, &status, 0);
 
-			if (execve("/bin/sh", args, NULL) == -1)
-			{
-				perror("execve");
-				_exit(EXIT_FAILURE);
-			}
-		}
-		else
+		if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
 		{
-			/* Parent process: wait for the child to complete*/
-			waitpid(pid, &status, 0);
-
-			if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
-			{
-				printf("Command failed to execute.\n");
-				/*error handling*/
-			}
-			/*
-			if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
-			printf("Command executed successfully.\n");
-			} else {
 			printf("Command failed to execute.\n");
-			}
-			*/
+			/* error handling */
 		}
 	}
-	/* Free dynamically allocated memory*/
+
 	free(command);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	bool eof_detected = false;
+
+	display_welcome_message();
+
+	while (!eof_detected)
+	{
+		char *command = read_command();
+
+		if (command == NULL)
+		{
+			eof_detected = true;
+		continue;
+		}
+
+		if (strcmp(command, "exit") == 0)
+		{
+			printf("Exiting...\n");
+			free(command);
+			break;
+		}
+
+		execute_command(command);
+	}
 
 	return (0);
-
-	}
+}
 
--- main.h
+++ main.h
@@ -5,15 +5,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include <unistd.h>  	 /* For fork, execve, exit, _exit, getpid, isatty, kill, read, write */ 
-#include <sys/types.h>  /* For wait, waitpid, wait3, wait4 */ 
-#include <sys/stat.h>   /* For stat, lstat, fstat*/ 
-#include <fcntl.h>      /* For open */ 
-#include <dirent.h>     /* For opendir, readdir, closedir*/ 
-#include <signal.h>     /* For signal*/ 
-#include <errno.h>      /* For perror*/ 
-#include <limits.h>     /* For PATH_MAX*/ 
-#include <sys/wait.h>   /* For waitpid */
+/* For fork, execve, exit, _exit, getpid, isatty, kill, read, write */
+#include <unistd.h>
+/* For wait, waitpid, wait3, wait4 */
+#include <sys/types.h>
+/* For stat, lstat, fstat*/
+#include <sys/stat.h>
+/* For open */
+#include <fcntl.h>
+/* For opendir, readdir, closedir*/
+#include <dirent.h>
+/* For signal*/
+#include <signal.h>
+/* For perror*/
+#include <errno.h>
+/* For PATH_MAX*/
+#include <limits.h>
+/* For waitpid */
+#include <sys/wait.h>
 
 
 #endif
